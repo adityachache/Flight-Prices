3d1e46df0ec06578b2024b59fbda41fe38f8286f
adityachache <aditya.chache@gmail.com>
2024-07-17 21:20:06 +0530

    Added new script which fetches data from Amadeus API

--- scripts/main.py
+++ scripts/main.py
@@ -1,29 +1,41 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+import os
 import pymongo
 from datetime import datetime
+from amadeus import Client, ResponseError
 from event_logger import EventLogger
+from dateutil.relativedelta import relativedelta
 
 HEADERS = {
-    "Accept-Language": "en-US,en;q=0.9",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
-    "Accept-Encoding": "gzip, deflate"
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Connection": "keep-alive"
 }
+
+BASE_URL_TOKEN = "https://test.api.amadeus.com/v1/security/oauth2/token/"
+
+API_KEY = os.getenv('API_KEY')
+API_SECRET = os.getenv('API_SECRET')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
+
+amadeus = Client(
+    client_id=API_KEY,
+    client_secret=API_SECRET
+)
+
+# Get the current date and time
+CURRENT_DATE = datetime.now()
 
 
 class DatabaseHandler:
     def __init__(self, db_name, collection_name):
         self.client = pymongo.MongoClient(
-            "mongodb+srv://adityachache:Slark_26@etlcluster.2itnsyc.mongodb.net/")
+            f"mongodb+srv://adityachache:{MONGO_PASSWORD}@etlcluster.2itnsyc.mongodb.net/")
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
 
     def insert_document(self, document):
         self.collection.insert_one(document)
+        print('data inserted in MongoDB')
 
     def find_documents(self, query):
         return self.collection.find(query)
@@ -32,131 +44,173 @@ class DatabaseHandler:
         return self.collection.find_one(query)
 
     def insert_many_documents(self, documents_list):
-        if not documents_list:
-            raise ValueError("documents_list is empty, cannot insert into MongoDB")
         self.collection.insert_many(documents_list, ordered=True)
         print('data inserted in MongoDB')
 
 
-class Scraper:
-    """Class to scrape flight details from skyscanner website"""
+class FlightOffers:
 
     def __init__(self):
         self.headers = HEADERS
+        self.resData = {}  # response from api will be loaded here
+        self.itineraries = list()
+        self.dataToSave = list()
 
     @staticmethod
-    def extract_data(data: list):
-        """extracts airline info from the scraped data and adds it into a python dictionary to save in mongoDB"""
-
+    def get_configvariables():
         handler = DatabaseHandler("ETL_Config", "ETL_ConfigVariables")
         handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
 
-        span_texts = []
-        data_to_save = []
-        source_city = {}
-        destination_city = {}
+        source_airport = ""
+        destination_airport = ""
+        max_offers = ""
 
         try:
-            source_city = handler.find_one_document({"name": "source_airport"})
-            destination_city = handler.find_one_document({"name": "destination_airport"})
+            source_airport = handler.find_one_document({"name": "source_IATA"})
+            destination_airport = handler.find_one_document({"name": "destination_IATA"})
+            max_offers = handler.find_one_document({"name": "maxOffers"})
         except Exception as e:
             event = EventLogger.log_event(e)
             handler_to_insert.insert_document(event)
 
-        if len(data) != 0:
-            try:
-                for obj in data:
-                    span_texts.append([span.get_text(strip=True) for span in obj.find_all('span')])
-            except Exception as e:
-                event = EventLogger.log_event(e)
-                handler_to_insert.insert_document(event)
+        obj = {
+            'source': source_airport["value"],
+            'destination': destination_airport["value"],
+            'max': max_offers["value"]
+        }
+
+        return obj
+
+    def get_flight_data(self):
+        handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
+
+        # Calculate the date one month from now
+        one_month_from_now = CURRENT_DATE + relativedelta(months=1)
+        one_month_from_now_str = one_month_from_now.strftime("%Y-%m-%d")
+
+        config_values = self.get_configvariables()
 
         try:
-            for item in span_texts:
-                # print(item)
-                price_per_passenger = "INR " + item[1].split()[1]  # ₹ 16,325 per passenger.
-                departure_date = item[2].split(',')[1].strip().replace('.', '')  # Tue, 7 May.
-                airline = item[3].split()[3:]  # Saudia.
-                airline = " ".join(airline).replace(".", "")
-                flight_type = item[3].split()[0]  # One-way.
-                airport = item[4].split(',')[-1].replace("arriving in ", "").replace(".", "").strip()  # Malpensa.
-                source = source_city['value']
-                destination = destination_city['value']
-                current_date = str(datetime.now().date())
-
-                data_obj = {'ticketCost': price_per_passenger, "departureDate": departure_date, "airline": airline,
-                            "flightType": flight_type, 'airport': airport, 'source': source,
-                            'destination': destination,
-                            'currentDate': current_date}
-                data_to_save.append(data_obj)
-
-        except Exception as e:
+            response = amadeus.shopping.flight_offers_search.get(
+                originLocationCode=config_values['source'],
+                destinationLocationCode=config_values['destination'],
+                departureDate=one_month_from_now_str,
+                adults=1,
+                max=config_values['max'],
+                nonStop='false',
+                currencyCode="INR")
+            # print(response.result)
+            self.resData = response.result
+        except ResponseError as e:
             event = EventLogger.log_event(e)
             handler_to_insert.insert_document(event)
 
-        return data_to_save
+    def format_data(self):
+        handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
+
+        try:
+            self.get_flight_data()
+            offerData = self.resData['data']
+        except KeyError as e:
+            event = EventLogger.log_event(e)
+            handler_to_insert.insert_document(event)
+            return  # Exit the function if there is a problem with the flight data
+
+        try:
+            for itineraries in offerData:
+                try:
+                    obj = {
+                        'itineraries': itineraries['itineraries'],
+                        'price': itineraries['price']
+                    }
+                    self.itineraries.append(obj)
+                except KeyError as e:
+                    event = EventLogger.log_event(e)
+                    handler_to_insert.insert_document(event)
+                    continue  # Skip to the next itinerary if there is a problem with the current one
+
+            for itinerary in self.itineraries:
+                try:
+                    newObj = {
+                        'totalDuration': itinerary['itineraries'][0]['duration'],
+                        'flights': itinerary['itineraries'][0]['segments'],
+                        'totalCost': {'currency': itinerary['price']['currency'], 'total': itinerary['price']['total']}
+                    }
+                    self.dataToSave.append(newObj)
+                except (KeyError, IndexError) as e:
+                    event = EventLogger.log_event(e)
+                    handler_to_insert.insert_document(event)
+                    continue  # Skip to the next itinerary if there is a problem with the current one
+        except Exception as e:
+            event = EventLogger.log_event(e)
+            handler_to_insert.insert_document(event)
+            return  # Exit the function if there is an unexpected error
+
+    def replace_codes(self):
+        handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
+
+        for entry in self.dataToSave:
+            for flight in entry['flights']:
+                try:
+                    # Replace carrier code with carrier name
+                    carrier_code = flight['carrierCode']
+                    flight['carrierCode'] = self.resData["dictionaries"]['carriers'][carrier_code]
+                    flight['operating']['carrierCode'] = self.resData["dictionaries"]['carriers'][
+                        flight['operating']['carrierCode']]
+                except KeyError as e:
+                    event = EventLogger.log_event(e)
+                    handler_to_insert.insert_document(event)
+
+                try:
+                    # Replace aircraft code with aircraft name
+                    aircraft_code = flight['aircraft']['code']
+                    flight['aircraft']['code'] = self.resData["dictionaries"]['aircraft'][aircraft_code]
+                except KeyError as e:
+                    event = EventLogger.log_event(e)
+                    handler_to_insert.insert_document(event)
+
+        return self.dataToSave
 
     @staticmethod
-    def save_data_to_db(data_array):
-        """Saves data into the database"""
-
+    def save_data_in_db(data):
         handler_to_add = DatabaseHandler("ETL_Data", "Airline_Data")
-        handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
-        try:
-            if not data_array:
-                raise ValueError("data_array is empty, cannot save to MongoDB")
-            handler_to_add.insert_many_documents(data_array)
-        except Exception as e:
-            event = EventLogger.log_event(e)
-            handler_to_insert.insert_document(event)
-
-    def get_deals(self):
-        """scrape data from the skyscanner website"""
-
-        source_iata_code = ""
-        destination_iata_code = ""
-        scraped_data = ""
-
-        handler = DatabaseHandler("ETL_Config", "ETL_ConfigVariables")
-        handler_to_insert = DatabaseHandler("ETL_Data", "Event_Log")
-
-        try:
-            source_iata_code = handler.find_one_document({"name": "source_IATA"})
-            destination_iata_code = handler.find_one_document({"name": "destination_IATA"})
-        except Exception as e:
-            event = EventLogger.log_event(e)
-            handler_to_insert.insert_document(event)
-
-        try:
-            web_url = f"https://www.skyscanner.co.in/routes/{source_iata_code['value']}/{destination_iata_code['value']}/"
-            service = Service('chromedriver.exe')  # Update path to chromedriver
-            options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--no-sandbox')
-            options.add_argument('--disable-dev-shm-usage')
-            driver = webdriver.Chrome(service=service, options=options)
-
-            driver.get(web_url)
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_all_elements_located((By.CLASS_NAME, "DealARIADescriptor_DealARIADescriptor__YWUwN"))
-            )
-            webpage = driver.page_source
-            # print(webpage)
-            soup = BeautifulSoup(webpage, "lxml")
-            # print("soup", soup)
-            scraped_data = soup.find_all("div", class_="DealARIADescriptor_DealARIADescriptor__YWUwN")
-            driver.quit()
-        except Exception as e:
-            event = EventLogger.log_event(e)
-            handler_to_insert.insert_document(event)
-
-        data_to_save = self.extract_data(scraped_data)
-        print("Data to save:", data_to_save)  # Add this line to check if data_to_save is populated
-
-        self.save_data_to_db(data_to_save)
+        handler_to_add.insert_many_documents(data)
 
 
 if __name__ == "__main__":
-    scraperObj = Scraper()
-    scraperObj.get_deals()
+    finder = FlightOffers()
+    finder.format_data()
+    finalData = finder.replace_codes()
+    print(finalData)
+    finder.save_data_in_db(finalData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
